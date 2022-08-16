ff7139ad73c444f4e1862833060c72685939c7e0
Pablo <pawel.stolka@gmail.com>
2022-08-16 12:58:06 +0200

    get input values

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  // @ViewChild('reason') reasonRef: ElementRef;
+  // @ViewChild('amount') amountRef//: ElementRef;
+  reasonRef;
+  amountRef;
+
   text = 'Ready to create an app?';
   defaultText = 'Ready to create an app?';
 
   constructor() {}
+
+  ngAfterViewInit() {
+
+  }
 
   onChangeText() {
     this.text = 'You did it :)';
@@ -18,5 +27,13 @@ export class HomePage {
       this.text = this.defaultText;
       console.log('default text set');
     }, 5000);
+  }
+
+  cancel() {
+    console.log('cancel');
+  }
+
+  confirm() {
+    console.log('confirm...',  this.reasonRef, this.amountRef); //, this.reasonRef.nativeElement, this.amountRef);
   }
 }
